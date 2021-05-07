e57649673d95f493b20798d11b13da4d32b2a65f
d7p4n4 <Szappan60>
2021-05-07 15:20:58 +0200

    1.1.20210507.1

--- convert.js
+++ convert.js
@@ -9,7 +9,7 @@ async function xmlToModdleJS(fileName) {
 
     try {
 
-        let xml = fs.fileReadSync("./xml/" + fileName, "utf-8");
+        let xml = fs.readFileSync("./xml/" + fileName, "utf-8");
 
         const {
             rootElement: definitions
@@ -25,6 +25,156 @@ async function xmlToModdleJS(fileName) {
 
 async function moddleJSToSyncfusionJS() {
 
-    let moddleObject = await xmlToModdleJS("osszetettebbPeldaGateway");
+    let moddleObject = await xmlToModdleJS("osszetettebbPeldaGateway.xml");
+
+    let moddleArray = moddleObject.diagrams[0].plane.planeElement;
+
+    console.log(moddleArray);
+
+    let nodeArray = [];
+    let connectorArray = [];
+
+    let syncfusionElements = await Enumerable.from(moddleArray)
+        .select((object) => {
+
+            let syObject = {};
+
+            if(object.$type.includes("Shape")) {
+
+                if (object.bpmnElement.$type.includes("Event")) {
+
+                    if (object.bpmnElement.$type.includes("Start")) {
+
+                        syObject = {
+                            id: object.bpmnElement.id, width: object.bounds.width, height: object.bounds.height,
+                            offsetX: object.bounds.x, offsetY: object.bounds.y,
+                            annotations: [{
+                                content: object.bpmnElement.name != undefined ? object.bpmnElement.name : ""
+                            }],
+                            shape: {
+                                type: 'Bpmn', shape: 'Event',
+                                event: {
+                                    event: 'Start'
+                                }
+                            }
+                        };
+
+                        nodeArray.push(syObject);
+
+                        return syObject;
+
+                    } else if (object.bpmnElement.$type.includes("End")) {
+
+                        syObject = {
+                            id: object.bpmnElement.id, width: object.bounds.width, height: object.bounds.height,
+                            offsetX: object.bounds.x, offsetY: object.bounds.y,
+                            annotations: [{
+                                content: object.bpmnElement.name != undefined ? object.bpmnElement.name : ""
+                            }],
+                            shape: {
+                                type: 'Bpmn', shape: 'Event',
+                                event: {
+                                    event: 'End'
+                                }
+                            }
+                        };
+
+                        nodeArray.push(syObject);
+
+                        return syObject;
+
+                    }
+
+                } else if (object.bpmnElement.$type.includes("Gateway")) {
+
+                    if (object.bpmnElement.$type.includes("Exclusive")) {
+
+                        syObject = {
+                            id: object.bpmnElement.id, width: object.bounds.width, height: object.bounds.height,
+                            offsetX: object.bounds.x, offsetY: object.bounds.y,
+                            annotations: [{
+                                content: object.bpmnElement.name != undefined ? object.bpmnElement.name : ""
+                            }],
+                            shape: {
+                                type: 'Bpmn', shape: 'Gateway',
+                                gateway: {
+                                    type: 'Exclusive'
+                                }
+                            }
+                        };
+
+                        nodeArray.push(syObject);
+
+                        return syObject;
+
+                    }
+
+                } else if (object.bpmnElement.$type.includes("Activity")) {
+
+                    if (object.bpmnElement.$type.includes("Call")) {
+
+                        syObject = {
+                            id: object.bpmnElement.id, width: object.bounds.width, height: object.bounds.height,
+                            offsetX: object.bounds.x, offsetY: object.bounds.y,
+                            annotations: [{
+                                content: object.bpmnElement.name != undefined ? object.bpmnElement.name : ""
+                            }],
+                            shape: {
+                                type: 'Bpmn', shape: 'Activity',
+                                activity: {
+                                    activity: 'SubProcess',
+                                    subProcess: {
+                                        boundary: "Call"
+                                    }
+                                }
+                            }
+                        };
+
+                        nodeArray.push(syObject);
+
+                        return syObject;
+
+                    }
+
+                } else if (object.bpmnElement.$type.includes("Task")) {
+
+                    if (object.bpmnElement.$type.includes("Service")) {
+
+                        syObject = {
+                            id: object.bpmnElement.id, width: object.bounds.width, height: object.bounds.height,
+                            offsetX: object.bounds.x, offsetY: object.bounds.y,
+                            annotations: [{
+                                content: object.bpmnElement.name != undefined ? object.bpmnElement.name : ""
+                            }],
+                            shape: {
+                                type: 'Bpmn', shape: 'Activity',
+                                activity: {
+                                    activity: 'Task',
+                                    task: {
+                                        type: "Service"
+                                    }
+                                }
+                            }
+                        };
+
+                        nodeArray.push(syObject);
+
+                        return syObject;
+
+                    }
+                }
+            } // if shape
+
+            if(object.$type.includes("Edge")) {
+
+
+
+            } // if edge
+
+        }).toArray() // syncfusionElements
+
+    console.log(nodeArray);
 
 } // moddleJSToSyncfusionJS
+
+moddleJSToSyncfusionJS();
